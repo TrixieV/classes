d74f2d26ffee51fd32f6d38a01979d5d2c241d9e
titi <thaophuong5625@gmail.com>
2023-07-31 16:51:28 +0200

    Adding homepage and several roles of login

---
+++ src/main/java/com/app/classes/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package com.app.classes.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails trixie = User.builder()
+                .username("trixie")
+                .password("{noop}trix123")
+                .roles("TEACHER")
+                .build();
+
+        UserDetails michelle = User.builder()
+                .username("michelle")
+                .password("{noop}mi123")
+                .roles("TEACHER")
+                .build();
+
+        UserDetails tina = User.builder()
+                .username("tina")
+                .password("{noop}tin123")
+                .roles("TEACHER", "MANAGER")
+                .build();
+
+        UserDetails max = User.builder()
+                .username("max")
+                .password("{noop}max123")
+                .roles("STUDENT")
+                .build();
+
+        UserDetails henry = User.builder()
+                .username("henry")
+                .password("{noop}henry123")
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(trixie, michelle, tina, max, henry);
+    }
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .anyRequest().authenticated())
+                .logout(logout -> logout.permitAll());
+        return http.build();
+    }
+}
